f1fabd7ba2eb25e507ef4602588ebb367ff6c44d
carlosargueta55 <101429659+carlosargueta55@users.noreply.github.com>
2023-03-02 13:18:29 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,24 @@
-!pip install openai 
+import requests
 
-openai.api_key = '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'
+URL = "https://api.openai.com/v1/chat/completions"
+
+payload = {
+"model": "gpt-3.5-turbo",
+"messages": [{"role": "user", "content": f"What is the first computer in the world?"}],
+"temperature" : 1.0,
+"top_p":1.0,
+"n" : 1,
+"stream": False,
+"presence_penalty":0,
+"frequency_penalty":0,
+}
+
+headers = {
+"Content-Type": "application/json",
+"Authorization": f"Bearer {openai.api_key}"
+}
+
+response = requests.post(URL, headers=headers, json=payload, stream=False)
      
-Method 1 - using OpenAI Python Package
 
-import openai
-
-messages = [
-    {"role": "system", "content": "You are a kind helpful assistant."},
-]
+response.content
