15fc6fe21bdf829ec1be271874336896565f1be5
carlosargueta55 <101429659+carlosargueta55@users.noreply.github.com>
2023-03-02 13:16:22 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,11 @@
-import streamlit as st
-st.write('Hello, world!')
+!pip install -q openai 
+
+openai.api_key = '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'
+     
+Method 1 - using OpenAI Python Package
+
+import openai
+
+messages = [
+    {"role": "system", "content": "You are a kind helpful assistant."},
+]
